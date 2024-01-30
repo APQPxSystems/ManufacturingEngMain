3fb7a3779ff8fffa879bbc89ca8bb0de3f70c306
APQPxSystems <157448855+APQPxSystems@users.noreply.github.com>
2024-01-30 15:37:25 +0800

    Update ManufacturingEngMain.py

--- ManufacturingEngMain.py
+++ ManufacturingEngMain.py
@@ -1,6 +1,7 @@
 # Full systems engineering web application
 
 # Import Libraries
+import hmac
 import streamlit as st
 import pandas as pd
 import altair as alt
@@ -17,6 +18,34 @@ hide_st_style = """
                 </style>
                 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+#-------------------------------------------------------------------------------------------
 
 # App title and info
 st.title("Manufacturing Engineering Dep't. Web App")
