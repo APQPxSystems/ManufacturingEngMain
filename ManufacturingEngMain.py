3fe0ed17df40aa28f3de0ff124b8901603720a5f
APQPxSystems <157448855+APQPxSystems@users.noreply.github.com>
2024-01-30 15:40:46 +0800

    Update ManufacturingEngMain.py

--- ManufacturingEngMain.py
+++ ManufacturingEngMain.py
@@ -38,7 +38,7 @@ def check_password():
 
     # Show input for password.
     st.text_input(
-        "Password", type="password", on_change=password_entered, key="password"
+        "ENTER PASSWORD TO OPEN THE WEB APP", type="password", on_change=password_entered, key="password"
     )
     if "password_correct" in st.session_state:
         st.error("😕 Password incorrect")
