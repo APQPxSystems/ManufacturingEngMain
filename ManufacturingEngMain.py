dffdeee1ef86ae41411edf5152e4771636bece42
APQPxSystems <157448855+APQPxSystems@users.noreply.github.com>
2024-02-01 17:41:11 +0800

    Update ManufacturingEngMain.py

--- ManufacturingEngMain.py
+++ ManufacturingEngMain.py
@@ -209,7 +209,7 @@ if automation_app == "Kigyo Generator":
     st.title("Kigyo Generator")
     st.write("""How to use: Upload required excel files.
              Drag the allowance slider to your desired price allowance in %.
-             The Kigyo output will be automatically generated
+             The Kigyo output will automatically generated
              """)
     st.subheader("Upload the required excel files")
 
@@ -236,10 +236,10 @@ if automation_app == "Kigyo Generator":
             st.write("Preview of Updated Inventory List:")
             inventory_list_df = pd.read_excel(inventory_list)
             st.write(inventory_list_df)
+    st.write("--------------------------------------------------------")
 
     # Generating the Kigyo Output
     if price_list is not None and parts_list is not None and inventory_list is not None:
-        st.write("--------------------------------------------------------")
         st.subheader("Generate Kigyo Excel File")
         percent_allowance = st.slider("Slide to the desired percent of price allowance", 0, 100, 10)
 
@@ -248,18 +248,15 @@ if automation_app == "Kigyo Generator":
         parts_price_inventory = parts_and_price.merge(inventory_list_df, how="left")
 
         # Kigyo Calculations
-        kigyo_ouput = parts_price_inventory[["Parts Type", "Parts List", "Price per Piece", "Quantity", "Quantity Available"]]
+        kigyo_ouput = parts_price_inventory[["Department", "Process", "Category Name", "Parts List", "Model No.", "Price per Piece", "Quantity", "Quantity Available"]]
         kigyo_ouput["Quantity to Purchase"] = (kigyo_ouput["Quantity"] - kigyo_ouput["Quantity Available"]).apply(lambda x: max(x, 0))
         kigyo_ouput["Purchase Cost"] = kigyo_ouput["Price per Piece"] * kigyo_ouput["Quantity to Purchase"]
         kigyo_ouput["Allowance"] = kigyo_ouput["Purchase Cost"] * (percent_allowance / 100)
         kigyo_ouput["Total Price"] = kigyo_ouput["Purchase Cost"] + kigyo_ouput["Allowance"]
         kigyo_ouput["Used Inventory"] = kigyo_ouput["Quantity"] - kigyo_ouput["Quantity to Purchase"]
         kigyo_ouput["Savings from Inventory"] = kigyo_ouput["Price per Piece"] * kigyo_ouput["Used Inventory"]
-        kigyo_ouput.set_index("Parts List", inplace=True)
-        st.write("--------------------------------------------------------")
 
     # Generating the Updated Inventory minus the Used Parts
-    if price_list is not None and parts_list is not None and inventory_list is not None:
         inventory_update = inventory_list_df.merge(parts_list_df, how="left")
         inventory_update["Quantity"] = inventory_update["Quantity"].fillna(0)
         inventory_update["New Quantity Available"] = (inventory_update["Quantity Available"] - inventory_update["Quantity"]).apply(lambda x: max(x, 0))
@@ -267,14 +264,15 @@ if automation_app == "Kigyo Generator":
         inventory_final_pd = pd.DataFrame(inventory_final)
         inventory_final_pd.rename(columns={"New Quantity Available":"Quantity Available"}, inplace=True)
         inventory_final_pd.set_index("Parts List", inplace=True)
+        st.write("--------------------------------------------------------")
 
         # Display Kigyo Output
-        st.subheader("Preview of Generated Kigyo")
+        st.subheader("Preview of generated Kigyo")
         st.write(kigyo_ouput)
         st.write("Total Purchase Cost: " + str(kigyo_ouput["Purchase Cost"].sum()))
         st.write("Total Allowance Cost: " + str(kigyo_ouput["Allowance"].sum()))
         st.write("Total Price (Cost + Allowance): " + str(kigyo_ouput["Total Price"].sum()))
-        st.write("Total Saved From Inventory: " + str(kigyo_ouput["Savings from Inventory"].sum()))
+        st.write("Total saved from inventory: " + str(kigyo_ouput["Savings from Inventory"].sum()))
 
         # Download Excel File
         @st.cache_data
@@ -290,8 +288,8 @@ if automation_app == "Kigyo Generator":
             mime="text/csv"
         )
         st.write("--------------------------------------------------------")
-      
-      # Diplay Updated inventory
+
+        # Display Generated Inventory Update
         st.subheader("Updated Inventory List")
         st.write(inventory_final_pd)
 
@@ -308,6 +306,61 @@ if automation_app == "Kigyo Generator":
             file_name="Updated Inventory.csv",
             mime="text/csv"
         )
+
+        st.write("--------------------------------------------------------")
+
+    # Generating the Table of Sub Kigyo No.
+        
+        st.subheader("Generating Sub Kigyo No. Output")
+        kigyo_sub_col1, kigyo_sub_col2, kigyo_sub_col3, kigyo_sub_col4 = st.columns([1.5,0.1,4,1.5])
+        
+        with kigyo_sub_col1:
+            # Assigning Kigyo Sub No. for each process
+            st.subheader("Sub Kigyo No.")
+            # Generating all unique processes
+            kigyo_output_copy = kigyo_ouput.copy()
+
+            unique_process = kigyo_output_copy["Process"].unique()
+
+            for process in unique_process:
+                # Get the selected Sub Kigyo No. for the current category
+                selected_sub_kigyo = st.selectbox(f"Select Sub Kigyo No. for {process}",
+                                                ("900", "901", "001", "002", "003", "004", "005",
+                                                "006", "007", "008", "009", "010"), key=process)
+                
+                # Add a new column "Sub Kigyo No." and set the selected value for the current category
+                kigyo_output_copy.loc[kigyo_output_copy["Process"] == process, "Sub Kigyo No."] = selected_sub_kigyo
+
+            # Now, kigyo_output_copy DataFrame contains the new column "Sub Kigyo No."
+                
+        with kigyo_sub_col3:
+            # Create final Sub Kigyo Output
+            sub_kigyo_output = kigyo_output_copy[["Process", "Category Name", "Sub Kigyo No."]]
+
+            # Renaming and adding some columns
+            sub_kigyo_output["Item Name"] = kigyo_output_copy["Parts List"]
+            sub_kigyo_output["Quantity"] = kigyo_output_copy["Quantity to Purchase"]
+            sub_kigyo_output["Average Price per Unit"] = kigyo_output_copy["Price per Piece"]
+            sub_kigyo_output["Price"] = sub_kigyo_output["Quantity"] * sub_kigyo_output["Average Price per Unit"]
+            
+            # Dropping rows with zero value in Quantity column
+            sub_kigyo_output = sub_kigyo_output[sub_kigyo_output["Quantity"] != 0]
+
+            # Rearranging Columns
+            sub_kigyo_output = sub_kigyo_output[["Process", "Category Name", "Item Name", "Average Price per Unit",
+                                                "Sub Kigyo No.", "Quantity", "Price"]]
+        
+            # Display Sub Kigyo Output
+            st.subheader("Sub Kigyo No. Output")
+            st.write(sub_kigyo_output)
+
+        with kigyo_sub_col4:        
+            # Get sum of price per unique values of Sub Kigyo No.
+            st.subheader("Subtotals")
+            sub_kigyo_totals = kigyo_output_copy.groupby("Sub Kigyo No.")["Total Price"].sum()
+            sub_kigyo_totals = pd.DataFrame(sub_kigyo_totals)
+            sub_kigyo_totals = sub_kigyo_totals[sub_kigyo_totals["Total Price"] != 0]
+            st.write(sub_kigyo_totals)
 
 # FMEA and QCP Matrix Date Calculator
 if automation_app == "FMEA and QCP Matrix Date Calculator":
