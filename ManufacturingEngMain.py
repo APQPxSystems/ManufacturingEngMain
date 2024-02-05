c578aa63e671092c311433290c0b283db851874b
APQPxSystems <157448855+APQPxSystems@users.noreply.github.com>
2024-02-05 10:36:47 +0800

    Update ManufacturingEngMain.py

--- ManufacturingEngMain.py
+++ ManufacturingEngMain.py
@@ -474,7 +474,7 @@ if automation_app == "FMEA PDCA Viewer":
     st.title("FMEA PDCA Viewer")
 
     # Read FMEA PDCA Excel File
-    fmea_pdca_raw = pd.read_csv("FMEA/FMEA_PDCA.csv")
+    fmea_pdca_raw = pd.read_excel("PDCA/PDCA.xlsx")
 
     # Drop Unnecessary Columns
     fmea_pdca_dropped_cols = fmea_pdca_raw[["Car Maker", "Car Model", "Line", "Findings",
