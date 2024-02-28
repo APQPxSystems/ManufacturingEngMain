12830363b6b5c5673f13d2c40db6bb4d1485cc0d
APQPxSystems <157448855+APQPxSystems@users.noreply.github.com>
2024-02-28 10:08:02 +0800

    Update ManufacturingEngMain.py

--- ManufacturingEngMain.py
+++ ManufacturingEngMain.py
@@ -34,12 +34,12 @@ st.markdown("""
         """, unsafe_allow_html=True)
 
 # App title and info
-st.markdown("<h2 style='text-align:center; background-color:#1e6262; font-family:Monaco'>MANUFACTURING ENGINEERING WEB APP</h2>", unsafe_allow_html=True)
+st.markdown("<p class='app_title'>MANUFACTURING ENGINEERING WEB APP</p>", unsafe_allow_html=True)
 
 
-st.markdown("""This web app is a collection of Manufacturing Engineering Department's automation tools.
+st.markdown("""<p class='app_info'>This web app is a collection of Manufacturing Engineering Department's automation tools.
           This runs on streamlit's cloud server and is not connected to any database.
-          Therefore, any data uploaded will not be saved or collected and will vanish everytime the app is refreshed.""")
+          Therefore, any data uploaded will not be saved or collected and will vanish everytime the app is refreshed.</p>""", unsafe_allow_html=True)
 
 # User Roles
 credential_col1, credential_col2 = st.columns([2,1])
