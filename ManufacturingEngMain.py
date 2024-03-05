2eb1942acccb896237eb692e28c906487d5e9317
APQPxSystems <157448855+APQPxSystems@users.noreply.github.com>
2024-03-05 13:42:12 +0800

    Update ManufacturingEngMain.py

--- ManufacturingEngMain.py
+++ ManufacturingEngMain.py
@@ -728,10 +728,10 @@ if automation_app == "Merge Master Sample Automation":
         st.write(raw_data)
     
         # Concatenate Column Contents -- Conn, AcceNo, ExteNo into a new column 'Conn'
-        raw_data['ConnNo'] = raw_data['ConnNo'].astype(str) + raw_data['AcceNo'].astype(str) + raw_data['ExteNo'].astype(str)
+        raw_data['Conn'] = raw_data['Conn'].astype(str) + raw_data['AcceNo'].astype(str) + raw_data['ExteNo'].astype(str)
     
         # Convert 'Conn' column values to integers, handling NaN values and non-numeric values
-        raw_data['ConnNo'] = pd.to_numeric(raw_data['ConnNo'].str.replace(r'[^0-9]', '', regex=True), errors='coerce', downcast='integer')
+        raw_data['Conn'] = pd.to_numeric(raw_data['Conn'].str.replace(r'[^0-9]', '', regex=True), errors='coerce', downcast='integer')
         #raw_data["Conn"] = raw_data["Conn"]/10
     
         # Drop the 'AcceNo' and 'ExteNo' columns
