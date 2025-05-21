f852bc9ac53f2f43399d487ceb46f6fd91878b8b
APQPxSystems <157448855+APQPxSystems@users.noreply.github.com>
2025-05-21 13:43:47 +0800

    Update app.py

--- MPPD/Wire_Data_Sorting/app.py
+++ MPPD/Wire_Data_Sorting/app.py
@@ -104,3 +104,7 @@ if uploaded_file:
         st.subheader("Output Table")
         st.dataframe(filtered_df)
 
+with open('style.css') as f:
+    css = f.read()
+
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
